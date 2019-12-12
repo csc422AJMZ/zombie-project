ea335da69c84a96c4613034f4929a87b76cf0916
xiongj15 <57078025+xiongj15@users.noreply.github.com>
2019-12-11 20:45:14 -0600

    Update FryingPan.java

--- zombie-project/zombie_project/FryingPan.java
+++ zombie-project/zombie_project/FryingPan.java
@@ -3,5 +3,5 @@ package zombie_project;
 public class FryingPan extends Weapon {
     //damage: (1-10)
     //accuracy: (2-5)
-    public FryingPan(){super(10, 1, "frying pan");}
+    public FryingPan(){super(10, 5, "frying pan");}
 }
